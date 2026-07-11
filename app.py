48e9aac153c2ed36eeda2618145e1e6c4cd62814
fghjkiin <guro_t2@guro.sen.hs.kr>
2026-07-11 16:51:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ with col1:
 with col2:
     if st.button("종료"):
         if st.session_state.start_time != 0:
-            ?? = time.time()
+            st.session_state.result = time.time()
             # 걸린 시간 계산 (종료 시간 - 시작 시간)
             st.session_state.result = ?? - ??
         else:
